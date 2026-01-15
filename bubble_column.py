beee312986665d0549ac0553910d583b8929acb0
Caspar Schwarz <caspar.s@gmx.de>
2026-01-15 14:42:32 +0100

    Added function to calculate e_g

--- bubble_column.py
+++ bubble_column.py
@@ -9,17 +9,52 @@ from statistics import mean
 from heat_exchanger import Heat_exchanger
 import numpy as np
 from icecream import ic
+from scipy.optimize import minimize
 
 class Bubble_Column(Heat_exchanger):
     
     def __init__(self, medium):
         super().__init__(self, medium)
         
-     
+    
+    def calc_e_g(self, D, sigma, v_g, C_1, rho_f=None, eta_f=None, g=None):
+        '''
         
-    def calc_a(self, rho_f, D, v_f, e_g, sigma, g=9.81):
+        C_1:        initial dissolved gas in kg/m^3
+        
+        '''
+        
+        
+        
+        A = C_1 * ((g* D**2 * rho_f)/sigma)**(1/8) \
+            * ((g * D**3 * rho_f**2)/eta_f**2)**(1/12) \
+            * (v_g/np.sqrt(g * D))
+            
+        f = lambda e_g: e_g / (1-e_g)**4 - A
+        
+        e_g_solved = minimize(f, 0.05).x
+        
+        return e_g_solved
+        
+    def calc_a(self, D, v_f, e_g, sigma, rho_f=None, g=9.81):
+        '''Calculates the volume specific area in m^2/m^3
+        
+        D:          diameter of the bubble column in m
+        v_f:        velocity of the fluid in m/s
+        e_g:        relative gas content in the fluid
+        sigma:      surface tension
+        rho_f:      density of the fluid in kg/m^3
+        
+        returns:
+        a:          volume speicifc area in m^2/m^3
+        '''
+        
+        if rho_f is None:
+            self.set_state_2()
         
         a = 1/(3*D) * ((g* D**2 * rho_f) / sigma)**(1/8) * ((g * D**3)/ v_f**2)**0.1 * e_g**1.13
+        
+        return a
         
      
     def calc_phi_z(self, phi_in, z, v_g, beta=None, a=None, **kwargs):
